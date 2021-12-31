58787271b57108e15aa8510481a194f836f90226
LunDien <kuhaudn@gmail.com>
2021-12-31 12:59:18 +0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://admin:lundien3003@cluster0.uw8aj.mongodb.net/v8supercars?retryWrites=true&w=majority');
+        mongoose.connect(
+            process.env.MONGODB_URI || 'mongodb+srv://admin:lundien3003@cluster0.uw8aj.mongodb.net/v8supercars?retryWrites=true&w=majority',
+            {
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+              useCreateIndex: true,
+              useFindAndModify: false,
+            },
+          );
         console.log('connect successfully')
     } catch (error) {
         console.log('connect failure')
