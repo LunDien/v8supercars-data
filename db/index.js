9771bca79a4727d9a569b2570b443679d60dce2a
LunDien <kuhaudn@gmail.com>
2021-12-31 13:16:54 +0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 
 async function connect() {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:lundien3003@cluster0.uw8aj.mongodb.net/v8supercars?retryWrites=true&w=majority')
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:lundien3003@cluster0.uw8aj.mongodb.net/v8supercars?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
                 .then(connect => console.log("connected to mongodb"))
                 .catch(e => console.log("error connecting to mongodb"))
 }
